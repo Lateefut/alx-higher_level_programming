3a9231b46dd5cd42d9d0cb88cf58bc506c370652
Adebanjo Lateef <adebanjolateef18@gmail.com>
2022-06-28 18:09:35 +0100

    program will assign different number when you run it

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,14 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    digit = number % (-10)
+    remainder = number % (-10)
 else:
-    digit = number % 10
-    if digit > 5:
-        print("Last digit of {} is {} and is greater than 5"
-                .format(number, digit))
-    elif digit == 0:
-        print("Last digit of {} is {} and is 0".format(number, digit))
-    else:
-        print("Last digit of {} is {} and is less than 6 and not 0"
-                .format(number, digit))
+    remainder = number % 10
+
+if remainder > 5:
+    print("Last digit of {} is {} and is greater than 5"
+          .format(number, remainder))
+elif remainder is 0:
+    print("Last digit of {} is {} and is 0"
+          .format(number, remainder))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0"
+          .format(number, remainder))
